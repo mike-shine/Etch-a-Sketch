bbbff013d612ab5b2d7b2a7e8043a8dd9cb47f2b
Mike Shine <michaelbshine@outlook.com>
2021-01-26 09:25:40 -0800

    created 16x16 grid with blue hover effect

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const body = document.querySelector('#body');
 
-for (let i = 0; i < 16; i++) {
+const container = document.createElement('div');
+container.style.border = '2px dotted red';
+container.classList.add('container');
+body.appendChild(container);
+
+const gridWidth = 16;
+
+for (let i = 0; i < gridWidth**2; i++) {
   let div = document.createElement('div');
-  div.style.border = '3px solid';
+  div.style.border = '1px solid';
+  div.classList.add('square');
   let br = document.createElement('br');
-  body.appendChild(div);
-  body.appendChild(br);
+  container.appendChild(div);
+  container.appendChild(br);
 
 }
