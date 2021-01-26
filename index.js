7e5dfbdf107b5e07a43e24e3a0a5862ccc1b1e69
Mike Shine <michaelbshine@outlook.com>
2021-01-26 09:56:06 -0800

    added user input field for sketchboard width, no functionality yet

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ container.style.border = '2px dotted red';
 container.classList.add('container');
 body.appendChild(container);
 
-const gridWidth = 16;
+const gridWidth = 25;
 
 for (let i = 0; i < gridWidth**2; i++) {
   let div = document.createElement('div');
