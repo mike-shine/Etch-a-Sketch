ce3c3ac768e0d3e1e26fbcb85d6f7a89665ae338
Mike Shine <michaelbshine@outlook.com>
2021-01-29 13:19:18 -0800

    renamed clear button to resize button, and added an actual clear button

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const body = document.querySelector('#body');
 
-const button = document.createElement('button');
-button.textContent = 'Clear grid';
-body.appendChild(button);
+const resizeButton = document.createElement('button');
+resizeButton.textContent = 'Resize grid';
+body.appendChild(resizeButton);
+
+const clearButton = document.createElement('button');
+clearButton.textContent = 'Clear grid';
+body.appendChild(clearButton);
 
 const container = document.createElement('div');
-// container.style.border = '2px dotted red';
 container.classList.add('container');
 body.appendChild(container);
+
 
 
 
@@ -16,7 +20,7 @@ function modifySize() {
   if (gridWidth > 100) {
     alert('Error: Too many squares! Please pick a value that is 100 or less.')
   } else {
-    clearGrid();
+    removeGrid();
     for (let i = 0; i < gridWidth**2; i++) {
       let div = document.createElement('div');
       div.style.border = '1px solid';
@@ -24,6 +28,9 @@ function modifySize() {
       let heightPixels = 1225 / gridWidth - 2;
       div.style.height = `${heightPixels}px`;
       div.style.width = div.style.height;
+      div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = "blue";
+      })
       container.appendChild(div);
     }
     let allSquares = document.querySelectorAll('square')
@@ -31,18 +38,26 @@ function modifySize() {
   console.log(document.querySelectorAll('square'));
 }
 
-function clearGrid() {
+function removeGrid() {
   let allSquares = document.getElementsByClassName('square');
   for (let i = allSquares.length - 1; i >= 0; i--) {
     allSquares[i].remove();
   }
 }
 
+function whitenAll() {
+  let allSquares = document.getElementsByClassName('square');
+  for (let i = allSquares.length - 1; i >= 0; i--) {
+    allSquares[i].style.backgroundColor = 'white';
+  }
+}
 
-modifySize()
+
+modifySize();
 
 
-button.addEventListener('click', modifySize);
+resizeButton.addEventListener('click', modifySize);
+clearButton.addEventListener('click', whitenAll);
 
 
 // figure out padding issue, of how to adjust padding when number of squares changes. Is my CSS just overwriting all changes I try to make?
